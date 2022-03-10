47ce855b13a1afcdd03aeaacd01a35aa5be89142
Junsu Shin <junsushin4546@gmail.com>
2022-03-10 23:17:24 +0900

    feat: Add basic time table for a week

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+const hours = new Array(24).fill(0).map((_, index) => index.toString().padStart(2, '0') + ":00");
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: 'flex' }}>
+      <div className='grid-column'>
+        <div className='grid-cell' />
+        {hours.map(hour => <div className='grid-cell'>{hour}</div>)}
+      </div>
+      {days.map(dayName => (
+        <div key={dayName} className='grid-column'>
+          <div className='grid-cell'>{dayName}</div>
+          {hours.map(hour => <div className='grid-cell'/>)}
+        </div>
+      ))}
     </div>
   );
 }
