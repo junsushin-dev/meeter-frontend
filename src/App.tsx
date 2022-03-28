f10cf8945adae74607338253607d6fbefb19d5ad
Junsu Shin <junsushin4546@gmail.com>
2022-03-28 21:33:16 +0900

    feat: Setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import CreateMeeting from "./pages/CreateMeeting";
+import MeetingDetail from "./pages/MeetingDetail";
+import SelectDate from "./pages/SelectDate";
 
 export default function App() {
-  return <div>hello</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<SelectDate />} />
+      <Route path="/create" element={<CreateMeeting />} />
+      <Route path="/meeting/:id" element={<MeetingDetail />} />
+    </Routes>
+  );
 }
