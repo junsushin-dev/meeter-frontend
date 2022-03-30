a34c56e8eb45272aaf5f1f6060dea7799a64a0f3
Junsu Shin <junsushin4546@gmail.com>
2022-03-30 22:31:55 +0900

    feat: Apply react-calendar to SelectDate component

--- src/pages/SelectDate.tsx
+++ src/pages/SelectDate.tsx
@@ -1,3 +1,13 @@
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
 export default function SelectDate() {
-  return <div>SelectDate</div>;
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  return (
+    <div>
+      <Calendar onChange={setSelectedDate} value={selectedDate} />
+    </div>
+  );
 }
