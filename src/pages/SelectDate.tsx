245bd0baea0118a4c09e5b57c33d0b0e27147617
Junsu Shin <junsushin4546@gmail.com>
2022-03-31 22:55:08 +0900

    feat: Enable multi select for calendar

--- src/pages/SelectDate.tsx
+++ src/pages/SelectDate.tsx
@@ -1,13 +1,36 @@
 import { useState } from "react";
-import Calendar from "react-calendar";
+import Calendar, { CalendarTileProperties } from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import { DateSet } from "../utils/toggleSet";
 
 export default function SelectDate() {
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedDateSet, setSelectedDateSet] = useState(
+    new DateSet([new Date()])
+  );
+
+  const onChange = (date: Date) => {
+    setSelectedDateSet((prevSet) => {
+      const newSet = new DateSet([...prevSet.values()]);
+      newSet.toggle(date);
+      return newSet;
+    });
+  };
+
+  const setTileClassName = ({ date, view }: CalendarTileProperties) => {
+    if (selectedDateSet.has(date)) {
+      return "react-calendar__tile--hasActive";
+    } else {
+      return null;
+    }
+  };
 
   return (
     <div>
-      <Calendar onChange={setSelectedDate} value={selectedDate} />
+      <Calendar
+        onChange={onChange}
+        value={null}
+        tileClassName={setTileClassName}
+      />
     </div>
   );
 }
