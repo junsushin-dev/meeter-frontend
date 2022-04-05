12298608792211f03f5610627cd563200d7424f5
Junsu Shin <junsushin4546@gmail.com>
2022-04-05 23:46:57 +0900

    feat: Add text input field for title and description

--- src/pages/CreateMeeting.tsx
+++ src/pages/CreateMeeting.tsx
@@ -1,5 +1,5 @@
 import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
-import { Box, Button, Stack, Typography } from "@mui/material";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
 import { BottomNavPaper } from "../components/BottomNavPaper";
 
@@ -10,6 +10,14 @@ export default function CreateMeeting() {
         <Typography component="h1" variant="h4">
           일정 생성 페이지
         </Typography>
+        <TextField id="meeting-title" label="Title" fullWidth />
+        <TextField
+          id="meeting-description"
+          label="Description"
+          multiline
+          fullWidth
+          rows={4}
+        />
       </Stack>
       <BottomNavPaper elevation={3}>
         <Button>
