5799d03159978e80afe4041d3334bd4252b82c39
Junsu Shin <junsushin4546@gmail.com>
2022-04-14 23:25:36 +0900

    feat: Bind controlled text inputs to store

--- src/pages/CreateMeeting.tsx
+++ src/pages/CreateMeeting.tsx
@@ -3,9 +3,11 @@ import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 import { BottomNavPaper } from "../components/BottomNavPaper";
+import { useStore } from "../stores";
 
 export default function CreateMeeting() {
   const navigate = useNavigate();
+  const { title, description, setTitle, setDescription } = useStore();
 
   const handleNextButtonClick = () => {
     // TODO: Apply meetingId path param
@@ -18,13 +20,21 @@ export default function CreateMeeting() {
         <Typography component="h1" variant="h4">
           일정 생성 페이지
         </Typography>
-        <TextField id="meeting-title" label="Title" fullWidth />
+        <TextField
+          id="meeting-title"
+          label="Title"
+          fullWidth
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
         <TextField
           id="meeting-description"
           label="Description"
           multiline
           fullWidth
           rows={4}
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
         />
       </Stack>
       <BottomNavPaper elevation={3}>
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,10 +2,19 @@ import create from "zustand";
 
 interface State {
   dates: Date[];
+  title: string;
+  description: string;
   setSelectedDates: (newDate: Date[]) => void;
+  setTitle: (newTitle: string) => void;
+  setDescription: (newDescription: string) => void;
 }
 
 export const useStore = create<State>((set) => ({
   dates: [] as Date[],
+  title: "",
+  description: "",
   setSelectedDates: (newDates: Date[]) => set((state) => ({ dates: newDates })),
+  setTitle: (newTitle: string) => set((state) => ({ title: newTitle })),
+  setDescription: (newDescription: string) =>
+    set((state) => ({ description: newDescription })),
 }));
