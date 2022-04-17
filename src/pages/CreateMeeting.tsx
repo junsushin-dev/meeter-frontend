3aeba461baa80a02f24cb5e712cc82675dcfe499
Junsu Shin <junsushin4546@gmail.com>
2022-04-17 21:04:29 +0900

    feat: Add timeRange input for CreateMeeting page

--- src/pages/CreateMeeting.tsx
+++ src/pages/CreateMeeting.tsx
@@ -1,5 +1,6 @@
 import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import { TimePicker } from "@mui/x-date-pickers";
 import { useNavigate } from "react-router-dom";
 
 import { BottomNavPaper } from "../components/BottomNavPaper";
@@ -7,7 +8,16 @@ import { useStore } from "../stores";
 
 export default function CreateMeeting() {
   const navigate = useNavigate();
-  const { title, description, setTitle, setDescription } = useStore();
+  const {
+    title,
+    description,
+    timeRangeStart,
+    timeRangeEnd,
+    setTitle,
+    setDescription,
+    setTimeRangeStart,
+    setTimeRangeEnd,
+  } = useStore();
 
   const handleNextButtonClick = async () => {
     // TODO: Apply meetingId path param
@@ -35,6 +45,18 @@ export default function CreateMeeting() {
           rows={4}
           value={description}
           onChange={(event) => setDescription(event.target.value)}
+        />
+        <TimePicker
+          label="시간 시작"
+          value={timeRangeStart}
+          onChange={setTimeRangeStart}
+          renderInput={(params) => <TextField fullWidth {...params} />}
+        />
+        <TimePicker
+          label="시간 종료"
+          value={timeRangeEnd}
+          onChange={setTimeRangeEnd}
+          renderInput={(params) => <TextField fullWidth {...params} />}
         />
       </Stack>
       <BottomNavPaper elevation={3}>
