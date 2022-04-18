8d1af63c5eec5806c90637c6ed600358d9399990
Junsu Shin <junsushin4546@gmail.com>
2022-04-18 20:16:05 +0900

    feat: Add CreateMeeting API call function

---
+++ src/apis/meetings.ts
@@ -0,0 +1,27 @@
+import { Dayjs } from "dayjs";
+
+interface CreateMeetingRequestParams {
+  meetingTitle: string;
+  meetingDesc: string;
+  availableDates: Date[];
+  timeRangeStart: Dayjs;
+  timeRangeEnd: Dayjs;
+}
+
+interface CreateMeetingResponse {
+  meetingUrlKey: string;
+}
+
+export const createMeeting = async (
+  params: CreateMeetingRequestParams
+): Promise<CreateMeetingResponse> => {
+  const response = await fetch("http://localhost:3001/api/meetings", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(params),
+  });
+
+  return response.json();
+};
